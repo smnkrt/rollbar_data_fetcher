babf7a92aa86c54d16d3e5e3062f7a94d50dad19
smnkrt <szymon.krywult@netguru.pl>
2019-01-24 18:41:15 +0100

    use SimpleStruct instead of Hash

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'uri'
 require 'net/http'
 require 'json'
@@ -6,44 +8,48 @@ require 'pry'
 
 require_relative 'simple_struct'
 
-MakeRequest = ->(url) do
-  http = Net::HTTP.new(url.host, url.port).tap { |h| h.use_ssl = true }
-  request = Net::HTTP::Get.new(url)
-  response = http.request(request)
+MakeRequest = lambda do |url|
+  request  = Net::HTTP::Get.new(url)
+  response = Net::HTTP.new(url.host, url.port)
+                      .tap { |h| h.use_ssl = true }
+                      .request(request)
   JSON.parse(response.read_body, object_class: SimpleStruct).result
 end
 
 FilterProjectsWithName = ->(result)   { result.reject { |r| r.name.nil? } }
-ExtractProjectId       = ->(projects) { projects.map { |r| { name: r.name, id: r.id } } }
+ExtractProjectId       = ->(projects) { projects.map { |r| SimpleStruct.new(name: r.name, id: r.id) } }
 
-FetchProjectsWithName  = ->(token) do
+FetchProjectsWithName  = lambda do |token|
   url = URI("https://api.rollbar.com/api/1/projects/?access_token=#{token}")
   (MakeRequest >> FilterProjectsWithName >> ExtractProjectId).call(url)
 end
 
-ExtractProjectAccessToken = ->(project, access_token) do
-  project_id = project.fetch(:id)
-  url = URI("https://api.rollbar.com/api/1/project/#{project_id}/access_tokens?access_token=#{access_token}")
-  token = MakeRequest.call(url).select { |e| e.scopes.include?("read") }.first.access_token
-  project.merge(token: token)
+ReadScopeFilter = ->(resource) { resource.scopes.include?('read') }
+
+ExtractProjectAccessToken = lambda do |project, access_token|
+  url = URI("https://api.rollbar.com/api/1/project/#{project.id}/access_tokens?access_token=#{access_token}")
+  token = MakeRequest.call(url).select { |e| e.scopes.include?('read') }.first.access_token
+  project[:token] = token
+  project
 end
 
-ExtractProjectDeploys = ->(project) do
-  project_token = project.fetch(:token)
-  url = URI("https://api.rollbar.com/api/1/deploys/?access_token=#{project_token}")
-  deploys = MakeRequest.call(url).deploys.select { |d| d.environment == "production" }
-  project.merge(latest_deploys: deploys)
+ProductionEnvFilter = ->(resource) { resource.environment == 'production' }
+
+ExtractProjectDeploys = lambda do |project|
+  url = URI("https://api.rollbar.com/api/1/deploys/?access_token=#{project.token}")
+  deploys = MakeRequest.call(url).deploys.select(&ProductionEnvFilter)
+  project[:deploys] = deploys
+  project
 end
 
-ExtractProjectItems = ->(project) do
-  project_token = project.fetch(:token)
-  url = URI("https://api.rollbar.com/api/1/items/?access_token=#{project_token}")
-  items = MakeRequest.call(url).items.select { |d| d.environment == "production" }
-  project.merge(latest_items: items)
+ExtractProjectItems = lambda do |project|
+  url = URI("https://api.rollbar.com/api/1/items/?access_token=#{project.token}")
+  items = MakeRequest.call(url).items.select(&ProductionEnvFilter)
+  project[:items] = items
+  project
 end
 
 projects = FetchProjectsWithName.call(ENV['ROLLBAR_ACCESS_TOKEN'])
+
 project  = ExtractProjectAccessToken.call(projects[0], ENV['ROLLBAR_ACCESS_TOKEN'])
 project  = ExtractProjectDeploys.call(project)
-
-binding.pry
