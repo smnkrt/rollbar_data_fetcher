468d6687e3f369df11899e6dad0d38c61e4fbf39
smnkrt <szymon.krywult@netguru.pl>
2019-01-25 09:22:29 +0100

    Mess with Load Path

--- lib/fetcher.rb
+++ lib/fetcher.rb
@@ -3,12 +3,14 @@
 require 'awesome_print'
 require 'pry'
 
-require_relative '../lib/make_request'
-require_relative '../lib/fetch_projects'
-require_relative '../lib/update_project_access_token'
-require_relative '../lib/production_env_filter'
-require_relative '../lib/update_projects_deploys'
-require_relative '../lib/update_project_items'
+$LOAD_PATH.unshift('lib')
+
+require 'make_request'
+require 'fetch_projects'
+require 'update_project_access_token'
+require 'production_env_filter'
+require 'update_projects_deploys'
+require 'update_project_items'
 
 projects = FetchProjects.call(ENV['ROLLBAR_ACCESS_TOKEN'])
 project  = UpdateProjectAccessToken.call(projects[0], ENV['ROLLBAR_ACCESS_TOKEN'])
