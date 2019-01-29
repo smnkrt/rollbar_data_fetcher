67780139fb5c9fd966271fc8e467194c28f9ec8b
smnkrt <szymon.krywult@netguru.pl>
2019-01-29 15:06:53 +0100

    handle case with no read scope token

--- lib/update_project_access_token.rb
+++ lib/update_project_access_token.rb
@@ -4,9 +4,7 @@ ReadScopeFilter = ->(resource) { resource.scopes.include?('read') }
 
 UpdateProjectAccessToken = lambda do |project, access_token|
   url = URI("https://api.rollbar.com/api/1/project/#{project.id}/access_tokens?access_token=#{access_token}")
-  token = MakeRequest.call(url)
-                     .select(&ReadScopeFilter)
-                     .first
-                     .access_token
-                     .then { |t| project.set(:token, t) }
+  MakeRequest.call(url)
+             .select(&ReadScopeFilter)
+             .then { |tokens| project.set(:token, tokens.first&.access_token) }
 end
